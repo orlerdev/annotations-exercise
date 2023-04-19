c20ba99ff5fdb81aedca325f4fa8ce5fffb4b760
Jared Orler <orlerdev@icloud.com>
2023-04-18 21:44:31 -0500

    fixed errors

--- src/Author.java
+++ src/Author.java
@@ -4,20 +4,13 @@
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("unchecked")
 public class Author extends Person {
-    private List books;
+    private List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
         books = new ArrayList();
-    }
-
-    /**
-     * @deprecated Use publishedBooks instead
-     */
-    @Deprecated
-    public List<String> getBooks() {
-        return books;
     }
 
     public List<String> publishedBooks() {
